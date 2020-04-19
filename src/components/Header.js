8b87d330abd188ebc587c11d17089e4edd20417d
AditiTrehan <adititrehan61@gmail.com>
2020-04-20 00:23:03 +0530

    Meme generator with Reacts hooks

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+    return(
+        <header>
+            <h1>Hey! Generate your own Meme!!</h1>
+        </header>
+    )
+}
+
+export default Header;
